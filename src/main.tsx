7380f81028b49931910aac7a31207442200c6da2
Saroj Ghalan <88360959+sarojghalan@users.noreply.github.com>
2023-05-14 12:26:13 +0545

    routing setup completed (#8)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { WrappedApp } from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <WrappedApp />
+    <BrowserRouter>
+      <WrappedApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
