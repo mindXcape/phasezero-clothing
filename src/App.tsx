501442767e179f5f8847ba37a1b4acfb8fcf6d97
Hypekage <jmestmnag@gmail.com>
2023-05-14 12:22:49 +0545

    routing setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Count from './Count';
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/count" element={<Count />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
 
 export function WrappedApp() {
-  return (
-    <HashRouter>
-      <App />
-    </HashRouter>
-  );
+  return <App />;
 }
