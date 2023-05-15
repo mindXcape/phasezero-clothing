3f1c97290d292ea64f25dbe0ad403b659bda0bdc
Prashishgt <prashish24@gmail.com>
2023-05-15 12:05:24 +0545

    PHS-33 converted previous model into typeScript and added comments and conditional rendering to modal>

--- src/Count.tsx
+++ src/Count.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ModalComponent from './components/modal/ModalComponent';
 
 function Count() {
   const [count, setCount] = useState<number>(0);
@@ -8,12 +9,14 @@ function Count() {
     setCount(count + 1);
   };
 
+  const desc = `Phasezero now offers you the convenience of being able to purchase items in your preferred currency. At checkout, all import duties and taxes will be applied to the final price.`;
   return (
     <>
       <p data-testid="count">{count}</p>
       <button type="button" onClick={addHandler}>
         Add
       </button>
+      <ModalComponent description={desc} />
     </>
   );
 }
