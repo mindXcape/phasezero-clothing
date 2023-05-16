d7f1dc4d7cd426bdc4c4afc19fd369e7969cf762
Hypekage <jmestmnag@gmail.com>
2023-05-16 20:49:16 +0545

    PHS-34 product card has been resolved

--- src/components/Card/ProductCard.tsx
+++ src/components/Card/ProductCard.tsx
@@ -2,7 +2,20 @@ import '../../scss/main.scss';
 import React, { useState } from 'react';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
-const Product = function Product() {
+// defining proptypes for product
+interface ProductProps {
+  imageFront: string;
+  imageBack: string;
+  title: string;
+  price: number;
+}
+
+const Product = function Product({
+  imageFront,
+  imageBack,
+  title,
+  price,
+}: ProductProps) {
   // state to handle the hover effect
   const [togglerState, setTogglerState] = useState<boolean>(false);
 
@@ -24,53 +37,45 @@ const Product = function Product() {
 
   return (
     <div
-      className="card_container"
+      className="card__container"
       onMouseEnter={(e) => togglerHandlerEnter(e)}
       onMouseLeave={(e) => togglerHandlerLeave(e)}
       onClick={(e) => navigateHandler(e)}
       aria-hidden="true"
     >
-      <div className="card_img">
+      <div className="card__img">
         {togglerState ? (
-          <img
-            src="https://cdn.shopify.com/s/files/1/0587/5816/8785/files/53Gn7CvMzuP8815M6SiPF_Foum-AdrNWBrqw3qI1gIw_540x.jpg?v=1682507128"
-            alt="end"
-          />
+          <img src={imageBack} alt="end" />
         ) : (
-          <img
-            src="https://cdn.shopify.com/s/files/1/0587/5816/8785/products/1_0c21b9f6-2b1d-4df2-9e13-0d406a90480e_540x.jpg?v=1632354544"
-            alt="front"
-          />
+          <img src={imageFront} alt="front" />
         )}
       </div>
 
       {togglerState ? (
-        <div className="card_btn_container">
+        <div className="card__btn__container">
           <div className="d-flex gap-2">
-            <div className="card_btn">
+            <div className="card__btn">
               <button type="button">S</button>
             </div>
-            <div className="card_btn">
+            <div className="card__btn">
               <button type="button">X</button>
             </div>
-            <div className="card_btn">
+            <div className="card__btn">
               <button type="button">L</button>
             </div>
-            <div className="card_btn">
+            <div className="card__btn">
               <button type="button">XL</button>
             </div>
           </div>
         </div>
       ) : (
-        <div className="card_content">
-          <p className="card_description">
-            Team 247 Oversized Hoodie x Marchon - Black
-          </p>
-          <p className="card_price">NPR 1200/-</p>
+        <div className="card__content">
+          <p className="card__description">{title}</p>
+          <p className="card__price">NPR {price}/-</p>
         </div>
       )}
 
-      <p className="card_icon">
+      <p className="card__icon">
         <FavoriteBorderIcon />
       </p>
     </div>
