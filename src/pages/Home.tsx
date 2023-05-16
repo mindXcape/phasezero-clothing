e5f9c6ddb9a605131ac700a53dd568f365615698
Hypekage <jmestmnag@gmail.com>
2023-05-16 19:37:16 +0545

    PHS-41 motistack setup completed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,19 @@
-function Home() {
-  return <h1>Hello World</h1>;
-}
+import React from 'react';
+import Message from '../Provider/Message';
+
+const Home: React.FC = function Home() {
+  // destructuring success message and error message of message provider
+  const { showSuccessMessage, showErrorMessage } = Message();
+  const messageHandler: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    // handling message
+    showErrorMessage('Error message');
+  };
+  return (
+    <button type="button" onClick={(e) => messageHandler(e)}>
+      Hello World
+    </button>
+  );
+};
 
 export default Home;
