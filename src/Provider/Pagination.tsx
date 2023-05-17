70ffff88bdf1c2425e12731054687ab749486a82
Hypekage <jmestmnag@gmail.com>
2023-05-17 18:28:26 +0545

    PHS-40 pagination component updated

--- src/Provider/Pagination.tsx
+++ src/Provider/Pagination.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
+// defining prop types for Pagination components
 interface PaginationProps {
   totalPage: number;
 }
@@ -9,6 +10,8 @@ interface PaginationProps {
 export default function BasicPagination(props: PaginationProps) {
   const { totalPage } = props;
   const [currentPage, setCurrentPage] = useState<number>(1);
+
+  // Update the current active page
   const handlePageChange = (
     event: React.ChangeEvent<unknown>,
     page: number
@@ -18,10 +21,10 @@ export default function BasicPagination(props: PaginationProps) {
   return (
     <Stack spacing={2}>
       <Pagination
-        count={totalPage}
+        count={totalPage} // Total number of pages
         color="primary"
-        page={currentPage}
-        onChange={handlePageChange}
+        page={currentPage} // Current active page
+        onChange={handlePageChange} // Event handler for page change
       />
     </Stack>
   );
