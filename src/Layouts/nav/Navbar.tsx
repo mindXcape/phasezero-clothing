ce87021ac8f96388ed6efc855d2c028e7e0bc6ef
subhashishshah <subhashish@ismt.edu.np>
2023-05-18 21:51:24 +0545

    Navbar completed with full responsiveness

--- src/Layouts/nav/Navbar.tsx
+++ src/Layouts/nav/Navbar.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import '../../scss/main.scss';
+import LocalMallIcon from '@mui/icons-material/LocalMall';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import SearchIcon from '@mui/icons-material/Search';
+import { NavLink } from 'react-router-dom';
+import { logo } from '../../assets';
+import Rightdrawers from '../../components/drawers/Rightdrawers';
 
-type Props = {
-  title: string;
-};
-
-function Navbar(props: Props) {
-  const { title } = props;
-  return <div>{title}</div>;
+function Navbar() {
+  return (
+    <div className="header__wrapper">
+      <div className="left__items">
+        <p>Shop</p>
+        <p>Collection</p>
+        <p>Community</p>
+      </div>
+      <NavLink to="/">
+        <img src={logo} alt="logo" className="logo" />
+      </NavLink>
+      <div className="right__section">
+        <div className="big__screen__item">
+          <p>Account</p>
+          <SearchIcon />
+          <FavoriteBorderIcon />
+          <LocalMallIcon />
+        </div>
+        <div className="right__drawer__wrapper">
+          <Rightdrawers />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
---
+++ src/components/drawers/Rightdrawers.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import MenuIcon from '@mui/icons-material/Menu';
+import '../../scss/main.scss';
+
+type Anchor = 'left';
+
+export default function TemporaryDrawer() {
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <div className="right__drawer__items">
+        <p>Shop by category</p>
+        <ChevronRightIcon className="icon" />
+      </div>
+    </Box>
+  );
+
+  return (
+    <div>
+      {(['left'] as const).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button
+            onClick={toggleDrawer(anchor, true)}
+            style={{ color: '#000' }}
+          >
+            <MenuIcon />
+          </Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
