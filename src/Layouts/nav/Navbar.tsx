9f72c85a79059787e67b141b655402425811418a
subhashishshah <subhashish@ismt.edu.np>
2023-05-19 11:40:30 +0545

    Worked on comments

--- src/Layouts/nav/Navbar.tsx
+++ src/Layouts/nav/Navbar.tsx
@@ -11,14 +11,17 @@ const menuItems = [{ text: 'Item 1' }, { text: 'Item 2' }, { text: 'Item 3' }];
 function Navbar() {
   return (
     <div className="header__wrapper">
+      {/* Left side items */}
       <div className="left__items">
         <p>Shop</p>
         <p>Collection</p>
         <p>Community</p>
       </div>
+      {/* Logo Image. Middle section of navbar */}
       <NavLink to="/">
         <img src={logo} alt="logo" className="logo" />
       </NavLink>
+      {/* Right section of navbar */}
       <div className="right__section">
         <div className="big__screen__item">
           <p>Account</p>
@@ -26,6 +29,7 @@ function Navbar() {
           <FavoriteBorderIcon />
           <LocalMallIcon />
         </div>
+        {/* This section gets enable in mobile view */}
         <div className="right__drawer__wrapper">
           <Rightdrawers menuItems={menuItems} />
         </div>
