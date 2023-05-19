48d0c88078d8017b43d378e59d7b192a30e448e3
Subhashish Shah <70791189+subhashishshah@users.noreply.github.com>
2023-05-19 13:35:01 +0545

    Phs 46 reconstruct navbar (#26)
    * Navbar completed with full responsiveness
    
    * Dynamic right drawer added with props.
    
    * Husky also configured and set as executable
    
    * Worked on comments

--- src/Layouts/nav/Navbar.tsx
+++ src/Layouts/nav/Navbar.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import '../../scss/main.scss';
+import LocalMallIcon from '@mui/icons-material/LocalMall';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import SearchIcon from '@mui/icons-material/Search';
+import { NavLink } from 'react-router-dom';
+import { logo } from '../../assets';
+import Rightdrawers from '../../components/drawers/Rightdrawers';
 
-type Props = {
-  title: string;
-};
+const menuItems = [{ text: 'Item 1' }, { text: 'Item 2' }, { text: 'Item 3' }];
 
-function Navbar(props: Props) {
-  const { title } = props;
-  return <div>{title}</div>;
+function Navbar() {
+  return (
+    <div className="header__wrapper">
+      {/* Left side items */}
+      <div className="left__items">
+        <p>Shop</p>
+        <p>Collection</p>
+        <p>Community</p>
+      </div>
+      {/* Logo Image. Middle section of navbar */}
+      <NavLink to="/">
+        <img src={logo} alt="logo" className="logo" />
+      </NavLink>
+      {/* Right section of navbar */}
+      <div className="right__section">
+        <div className="big__screen__item">
+          <p>Account</p>
+          <SearchIcon />
+          <FavoriteBorderIcon />
+          <LocalMallIcon />
+        </div>
+        {/* This section gets enable in mobile view */}
+        <div className="right__drawer__wrapper">
+          <Rightdrawers menuItems={menuItems} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
