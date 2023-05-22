36976688a3c97b5a4ff11a0cf87e61727e502582
Hypekage <jmestmnag@gmail.com>
2023-05-22 17:04:32 +0545

    PHS-63 ready for design approval

--- src/Routes/HomeRoutes.tsx
+++ src/Routes/HomeRoutes.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';HO
+import Home from '../pages/Home';
 import Count from '../Count';
 import Checkout from '../pages/Checkout';
 import Contact from '../pages/Contact';
 import NotFound from '../pages/NotFound';
 import CollectionPage from '../pages/CollectionPage';
+import ShopAll from '../pages/ShopAll';
 
 const HomeRoutes = function HomeRoutes() {
   return (
@@ -15,6 +16,7 @@ const HomeRoutes = function HomeRoutes() {
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/collections" element={<CollectionPage />} />
+      <Route path="/shop" element={<ShopAll />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
