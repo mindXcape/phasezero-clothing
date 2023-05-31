da232932149bc33ff775dc81e94fb41178a65cd3
subhashishshah <subhashish@ismt.edu.np>
2023-05-31 16:21:26 +0545

    Updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HomeRoutes from './Routes/HomeRoutes';
-import Navbar from './Layouts/nav/Navbar';
-import Footer from './Layouts/footer/Footer';
 
 export default function App() {
   // return (
