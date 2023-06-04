60ef7e8ad526fd7fe834c01877df05cd0cbf81c2
Hypekage <jmestmnag@gmail.com>
2023-06-04 14:36:14 +0545

    cart button refined

--- src/pages/Cartpage.tsx
+++ src/pages/Cartpage.tsx
@@ -30,18 +30,6 @@ function CartPage() {
                 quantity={1}
                 price={2200}
               />
-              <CartTable
-                image="https://cdn.shopify.com/s/files/1/0587/5816/8785/products/1_0c21b9f6-2b1d-4df2-9e13-0d406a90480e_540x.jpg?v=1632354544"
-                product_title="Spring 2023"
-                quantity={1}
-                price={2200}
-              />
-              <CartTable
-                image="https://cdn.shopify.com/s/files/1/0587/5816/8785/products/1_0c21b9f6-2b1d-4df2-9e13-0d406a90480e_540x.jpg?v=1632354544"
-                product_title="Spring 2023"
-                quantity={1}
-                price={2200}
-              />
             </div>
           </div>
           <div className="col-md-4">
