4d14bf40fc2bbe946cce963bc68d62787fee9e2a
subhashishshah <subhashish@ismt.edu.np>
2023-06-05 22:38:22 +0545

    Added new slider section for mobile view inside image gallery section

--- src/Layouts/Product-Detail/ImageGallery.tsx
+++ src/Layouts/Product-Detail/ImageGallery.tsx
@@ -1,22 +1,28 @@
 import { athleisureback, athleisurefront } from '../../assets';
+import SliderComponent from '../../components/Sliders/SliderComponent';
 import '../../scss/main.scss';
 
 function ImageGallery() {
   return (
-    <div className="product__detail__gallery row">
-      <div className="gallery__image col-lg-6">
-        <img className="images" src={athleisureback} alt="gallery " />
+    <>
+      <div className="product__detail__gallery row">
+        <div className="gallery__image col-lg-6">
+          <img className="images" src={athleisureback} alt="gallery " />
+        </div>
+        <div className="gallery__image col-lg-6">
+          <img className="images" src={athleisurefront} alt="" />
+        </div>
+        <div className="gallery__image col-lg-6">
+          <img className="images" src={athleisureback} alt="gallery " />
+        </div>
+        <div className="gallery__image col-lg-6">
+          <img className="images" src={athleisurefront} alt="" />
+        </div>
       </div>
-      <div className="gallery__image col-lg-6">
-        <img className="images" src={athleisurefront} alt="" />
+      <div className="gallery__mobile__view">
+        <SliderComponent />
       </div>
-      <div className="gallery__image col-lg-6">
-        <img className="images" src={athleisureback} alt="gallery " />
-      </div>
-      <div className="gallery__image col-lg-6">
-        <img className="images" src={athleisurefront} alt="" />
-      </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Sliders/SliderComponent.tsx
@@ -0,0 +1,36 @@
+import { athleisureback, athleisurefront } from '../../assets';
+
+function SliderComponent() {
+  return (
+    <div id="carouselExample" className="carousel slide">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={athleisureback} className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src={athleisurefront} className="d-block w-100" alt="..." />
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExample"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true" />
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExample"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true" />
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+}
+
+export default SliderComponent;
