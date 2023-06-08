8dee64b5c540eaa3d3664ae5273521b96123a453
Hypekage <jmestmnag@gmail.com>
2023-06-08 19:59:14 +0545

    PHS-80 athelisure section refined

--- src/Layouts/Athleisure/Athleisure.tsx
+++ src/Layouts/Athleisure/Athleisure.tsx
@@ -17,12 +17,12 @@ function Athleisure() {
             <div className="col-12 col-md-12 col-lg-3 col-sm-6">
               <div className="athleisure__image__container">
                 <div className="row">
-                  <div className="col-md-6 col-12 col-lg-12 ">
+                  <div className="col-md-6 col-6 col-lg-12 ">
                     <div className="athleisure__image mb-3">
                       <img src={athleisureback} alt="" />
                     </div>
                   </div>
-                  <div className="col-md-6 col-12 col-lg-12">
+                  <div className="col-md-6 col-6 col-lg-12">
                     <div className="athleisure__image">
                       <img src={athleisurefront} alt="" />
                     </div>
