204f938e43ec04214a4f39e0cc59a76ea42c65db
Hypekage <jmestmnag@gmail.com>
2023-06-19 23:34:03 +0545

    PHS-84 margin padding adjusted

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -9,25 +9,29 @@ import phone_screen from './phone_screen.jpg';
 import pz_lifestyle1 from './pz_lifestye1.jpg';
 import pz_lifestyle2 from './pz_lifestyle2.jpg';
 import carouselfinal from './carouselfinal.jpg';
-import first from './first (11).jpg';
 import second from './second.jpg';
 import third from './third.jpg';
 import fourth from './fourth.jpg';
 import fivth from './five.jpg';
 import sixth from './six.jpg';
 import seventh from './seven.jpg';
+import seven from './seventh.jpg';
 import eighth from './eight.jpg';
 import ninth from './eightt.jpg';
 import tenth from './first (1).jpg';
+import artist from './aforartist.jpg';
+import lifestyle from './lifestyle.jpg';
 
 export {
-  first,
+  artist,
+  lifestyle,
   second,
   third,
   fourth,
   fivth,
   sixth,
   seventh,
+  seven,
   eighth,
   ninth,
   tenth,
