b6fc14ce41cb55a197bef65528dc60990ec46683
Prashish G.T <120195519+Prashishgt@users.noreply.github.com>
2023-06-19 21:32:46 +0545

    Phs 87 community modal design (#44)
    * PHS-87 community modal complted waiting for design approval
    
    * PHS-87-Community-Modal-Design, design approved and waiting for code review
    
    * PHS-87 code review completed
    
    * PHS-87 changes done with code review

--- src/Layouts/Community/CommunityGrid.tsx
+++ src/Layouts/Community/CommunityGrid.tsx
@@ -1,48 +1,68 @@
-import React from 'react';
-import { bigbannerimg, displaypic } from '../../assets';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/no-array-index-key */
+import React, { useState } from 'react';
+import { ImageType, images } from '../../data/images';
+import CommunityModal from '../../components/ui/modal/CommunityModal';
 
 function CommunityGrid() {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(
+    null
+  );
+
+  const openModal = (index: number) => {
+    setSelectedImageIndex(index);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setSelectedImageIndex(null);
+    setIsModalOpen(false);
+  };
+
+  const goToNextImage = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (selectedImageIndex !== null && selectedImageIndex < images.length - 1) {
+      setSelectedImageIndex(selectedImageIndex + 1);
+    }
+  };
+
+  const goToPrevImage = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (selectedImageIndex !== null && selectedImageIndex > 0) {
+      setSelectedImageIndex(selectedImageIndex - 1);
+    }
+  };
+
   return (
-    <div className="community__grid__wrapper">
-      <div className="grid__post__item">
-        <img
-          className="community__grid__item__image"
-          src={bigbannerimg}
-          alt=""
+    <>
+      <div className="community__grid__wrapper">
+        {images.map((item: ImageType, index: number) => (
+          <button
+            key={`image-${index}`}
+            type="button"
+            className="grid__post__item"
+            onClick={() => openModal(index)}
+          >
+            <img
+              className="community__grid__item__image"
+              src={item.image}
+              alt="Community Post"
+            />
+          </button>
+        ))}
+      </div>
+      {selectedImageIndex !== null && (
+        <CommunityModal
+          selectedImage={images[selectedImageIndex].image}
+          isOpen={isModalOpen}
+          onClose={closeModal}
+          goToNextImage={goToNextImage}
+          goToPrevImage={goToPrevImage}
         />
-      </div>
-      <div className="grid__post__item">
-        <img
-          className="community__grid__item__image"
-          src="https://scontent.cdninstagram.com/v/t51.29350-15/352642049_655550593278091_2676964400428628937_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=djBfs2IBDKwAX9gLd1O&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&oh=00_AfAJ2vuFDL5B8Y3QF2GhdSZt7537lNrFJQga2sD4nJ6zMQ&oe=648F7795"
-          alt=""
-        />
-      </div>
-      <div className="grid__post__item">
-        <img
-          className="community__grid__item__image"
-          src="https://scontent.cdninstagram.com/v/t51.29350-15/351453617_1505737326901567_3047504388768435149_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=eROQqAkOrC8AX_072XE&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&oh=00_AfAxTBrb9aZt99fYZ75lpdldrZZmqTMXhf5CEh0rfklRrA&oe=648F92B4"
-          alt=""
-        />
-      </div>
-      <div className="grid__post__item">
-        <img className="community__grid__item__image" src={displaypic} alt="" />
-      </div>
-      <div className="grid__post__item">
-        <img
-          className="community__grid__item__image"
-          src="https://scontent.cdninstagram.com/v/t51.2885-15/351147361_787793712815546_7743206724008841251_n.jpg?stp=dst-jpg_e35_p480x480&_nc_ht=scontent.cdninstagram.com&_nc_cat=102&_nc_ohc=GtaS1hPxmKgAX9TFrc1&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AfBbN9BkYQzw_GaePf3COsdAXqO4C90cUpryRlzbMZklVw&oe=648B4AE3&_nc_sid=045e46"
-          alt=""
-        />
-      </div>
-      <div className="grid__post__item">
-        <img
-          className="community__grid__item__image"
-          src="https://scontent.cdninstagram.com/v/t51.29350-15/352217316_967607827728415_8079031931959249824_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=87N7BOC2huYAX-eR9Wi&_nc_ht=scontent.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AfB_WbNUQ2xCxpwgrsxVM9zijhkovjk_z3mOwU03gPZEww&oe=64903D72"
-          alt=""
-        />
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
