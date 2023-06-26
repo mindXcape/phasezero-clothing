2d93483b3f80cd93d139881e7557309205a5ba25
Prashishgt <prashish24@gmail.com>
2023-06-26 06:57:17 +0545

    PZA-22 Setup axios for frontend

---
+++ src/hooks/useAxios.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosInstance } from 'axios'
+
+interface IUseAxios {
+  (): AxiosInstance
+}
+
+const useAxios: IUseAxios = () => {
+  const headers = { 'content-type': 'application/json' }
+
+  const axiosCall = axios.create({
+    baseURL: process.env.BASE_URL,
+    headers,
+  })
+
+  axiosCall.interceptors.response.use(
+    (response) => response,
+    (error) => Promise.reject(error)
+  )
+
+  return axiosCall
+}
+
+export default useAxios
